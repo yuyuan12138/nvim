7b16a19220cf89223eb4c7989d66da756d5bf9ff
Ruojun Wang <yuyuan12138x@gmail.com>
2025-12-31 17:00:59 +0800

    breifly config snippets

---
+++ code-snippets/test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    
+
+    return 0;
+}
--- lua/plugin-config/LuaSnip.lua
+++ lua/plugin-config/LuaSnip.lua
@@ -1,14 +1,17 @@
-
 local M = {
     "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
     tag = "v2.*",
     run = "make install_jsregexp"
 }
 
 function M.config() 
-    require "luasnip.loaders.from_vscode".lazy_load {
-    
-    }
+    require("luasnip.loaders.from_vscode").lazy_load ({
+
+    })
+    require("luasnip.loaders.from_vscode").load_standalone({
+        path="~/.config/nvim/code-snippets/acm.code-snippets"
+    })
 end
 
 
